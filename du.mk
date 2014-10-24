05979cd6fbd71a97eb26b44d3ae7bc4f710263b5
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-24 19:19:55 -0400

    jflte: inherit APNs
    Change-Id: I2e8b6c3ef085dc72779337906acc7ead688af598

--- du.mk
+++ du.mk
@@ -1,6 +1,9 @@
 $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common DU stuff.
 $(call inherit-product, vendor/du/config/common.mk)
+
+# Inherit the APNs
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 PRODUCT_NAME := du_jflte
